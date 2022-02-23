1f3d54a788a0563848cb8458605af524501058ec
ksantos16 <u121364866@byui.edu>
2022-02-22 22:14:53 -0500

    added gem + 1 or rock -1, not sure sure if that will work

--- director.py
+++ director.py
@@ -60,8 +60,8 @@ class Director:
 
         for artifact in artifacts:
             if robot.get_position().equals(artifact.get_position()):
-                self.score += artifact.get_gem()
-                self.score += artifact.get_rock()
+                self.score += artifact.get_gem() + 1
+                self.score += artifact.get_rock() - 1
                 self.total_score += self.score
 
     def _do_outputs(self, cast):
