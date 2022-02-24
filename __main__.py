0b9905fb9b83f9f0ec7040001e6e951187b5b780
ksantos16 <u121364866@byui.edu>
2022-02-23 23:23:14 -0500

    made change to the artifacts by adding gem and rock

--- __main__.py
+++ __main__.py
@@ -14,15 +14,15 @@ from game.shared.color import Color
 from game.shared.point import Point
 
 
-FRAME_RATE = 12
+FRAME_RATE = 6
 MAX_X = 900
 MAX_Y = 600
 CELL_SIZE = 15
 FONT_SIZE = 15
 COLS = 60
 ROWS = 40
-CAPTION = "Robot Finds Kitten"
-DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
+CAPTION = "Greed"
+# DATA_PATH = os.path.dirname(os.path.abspath(__file__)) + "/data/messages.txt"
 WHITE = Color(255, 255, 255)
 DEFAULT_ARTIFACTS = 40
 
@@ -52,18 +52,18 @@ def main():
     robot.set_position(position)
     cast.add_actor("robots", robot)
 
-    # create the artifacts
-    with open(DATA_PATH) as file:
-        data = file.read()
-        messages = data.splitlines()
+    # # create the artifacts
+    # with open(DATA_PATH) as file:
+    #     data = file.read()
+    #     messages = data.splitlines()
 
     for n in range(DEFAULT_ARTIFACTS):
         gem = "*"
         rock = "O"
-        message = messages[n]
+        # message = messages[n]
 
-        x = gem
-        y = rock
+        x = random.randint(1, COLS - 1)
+        y = random.randint(1, ROWS - 1)
         position = Point(x, y)
         position = position.scale(CELL_SIZE)
 
@@ -73,11 +73,12 @@ def main():
         color = Color(r, g, b)
 
         artifact = Artifact()
-        artifact.set_text(text)
+        artifact.set_text(gem)
+        artifact.set_text(rock)
         artifact.set_font_size(FONT_SIZE)
         artifact.set_color(color)
         artifact.set_position(position)
-        artifact.set_message(message)
+        # artifact.set_message(message)
         cast.add_actor("artifacts", artifact)
 
     # start the game
